8f1a245039e1bd50b35a40e340468a147a6a572a
Nikolaos Karavasilis <nkaravasilis@wings-ict-solutions.eu>
2024-06-25 01:27:36 +0300

    fastapi service load best model

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ import os
 class Settings:
     MODEL_NAME: str = "Bert-cased"
     MODEL_PATH: str = os.getenv(
-        "MODEL_PATH", "weights/model_epoch1.pth"
+        "MODEL_PATH", "weights/best.pth"
     )  # if env is not set, it defaults to the best weights
     NUM_LABELS: int = 15
     MAX_LENGTH: int = 100
